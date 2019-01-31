4c1dfab2d74af87eb8d18e00d621b204ffebaf29
AlexAlper <Kalter1320@yandex.ru>
2019-01-31 17:53:40 +0300

    add listview for state

--- app/src/main/java/com/example/androidlesson/MainActivity.java
+++ app/src/main/java/com/example/androidlesson/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidlesson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        String[] cityNames = getResources().getStringArray(R.array.city_names);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, cityNames);
+
+        listView.setAdapter(adapter);
     }
+
 }
