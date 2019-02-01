6632df4c8f128fe18f45d9003e9ae4c9aa9d0c54
AlexAlper <Kalter1320@yandex.ru>
2019-02-02 00:54:01 +0300

    Сделал задание для 3 задания. Хотя дизайн выглядит как-то криво

--- app/src/main/java/com/example/androidlesson/MainActivity.java
+++ app/src/main/java/com/example/androidlesson/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.androidlesson;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +21,47 @@ public class MainActivity extends AppCompatActivity {
 
 
         ListView listView = (ListView)findViewById(R.id.listView);
+        final TextView choice = (TextView) findViewById(R.id.choice);
 
-        String[] cityNames = getResources().getStringArray(R.array.city_names);
+        final String[] cityNames = getResources().getStringArray(R.array.city_names);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, cityNames);
 
         listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View itemClicked, int position,
+                                    long id) {
+                // по позиции получаем выбранный элемент
+                String selectedItem = cityNames[position];
+                // установка текста элемента TextView
+                choice.setText(selectedItem);
+            }
+        });
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+             //   Intent intent = new Intent(this, CityActivity.class);
+               TextView txt = (TextView) findViewById(R.id.choice);
+                Intent intent = new Intent(this, CityActivity.class);
+                intent.putExtra("city",txt.getText().toString());
+
+
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
 
 }
